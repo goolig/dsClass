8025188b1d0bcf45a9aafe72050e680cd7697640
guy <shtar@post.bgu.ac.il>
2018-09-26 16:20:58 +0300

    <commit message>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ds_class',
+    version=":ds_class:a",
+    description='files to be used in the class',
+    author='Guy Shtar',    
+    url='https://github.com/goolig/ds_class/tree/master/ds_class',
+    license='MIT',    
+    include_package_data=True,
+    zip_safe=False,
+)
