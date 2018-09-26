7994fe1adbeca4d8c11b8107897ceeed8c5279dc
guy <shtar@post.bgu.ac.il>
2018-09-26 16:30:06 +0300

    lkj

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dsClass',
-    version=":dsClass:a",
+    name='dsClass',    
     description='files to be used in the class',
     author='Guy Shtar',    
     url='https://github.com/goolig/ds_class/tree/master/ds_class',
-    license='MIT',    
-    include_package_data=True,
-    zip_safe=False,
+    include_package_data=True,    
 )
