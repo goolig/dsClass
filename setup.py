2f79b0b1b166075fb8020aa39e461523a51c7fca
guy <shtar@post.bgu.ac.il>
2018-09-27 12:28:19 +0300

    dfg

--- setup.py
+++ setup.py
@@ -6,5 +6,8 @@ setup(
     description='A useful module',
     author='Guy',
     author_email='example@example.com',
+    install_requires=[
+          'pydotplus',
+      ],
     packages=['dsClass'],  #same as name
 )
