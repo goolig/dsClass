e64c39a24064bf3bc27abc9370aa75f7d18f0245
guy <shtar@post.bgu.ac.il>
2018-09-27 14:59:52 +0300

    adding data

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup
 
 setup(
     name='dsClass',
-    version='1.0',
+    version='1.0.14',
     description='A useful module',
     author='Guy',
     author_email='example@example.com',
     install_requires=[
           'pydotplus',
       ],
+    include_package_data=True,
     packages=['dsClass'],  #same as name
+    package_data={'': ['*.csv'],},
 )
