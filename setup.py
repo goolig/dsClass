324d707a2e6c7c35d71c13e6520b6512132630ec
guy <shtar@post.bgu.ac.il>
2018-10-07 17:06:11 +0300

    Adding data for class 1+2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dsClass',
-    version='1.0.18',
+    version='1.0.19',
     packages=find_packages(),
     description='A useful module',
     author='Guy',
