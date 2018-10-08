b62ada59c7f4a2f5e002c2c62af798217e1e7d3d
guy <shtar@post.bgu.ac.il>
2018-10-08 12:11:22 +0300

    updating version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dsClass',
-    version='1.0.19',
+    version='1.0.20',
     packages=find_packages(),
     description='A useful module',
     author='Guy',
