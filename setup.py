71673e952f860f0d8199c4f7379d6dc19173e881
guy <shtar@post.bgu.ac.il>
2018-10-08 12:57:44 +0300

    adding data task 7 + 8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dsClass',
-    version='1.0.22',
+    version='1.0.23',
     packages=find_packages(),
     description='A useful module',
     author='Guy',
