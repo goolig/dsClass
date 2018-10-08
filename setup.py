7211ba98e6ea6256230b1560b37898d08e2e187f
guy <shtar@post.bgu.ac.il>
2018-10-08 12:25:55 +0300

    updating the setup to allow pickle files as data

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dsClass',
-    version='1.0.20',
+    version='1.0.21',
     packages=find_packages(),
     description='A useful module',
     author='Guy',
@@ -11,5 +11,5 @@ setup(
           'pydotplus',
       ],
     include_package_data=True,
-    package_data={'': ['*.csv'],},
+    package_data={'': ['*.csv'],'2': ['*.pickle']},
 )
