a5672e7addf8f4d19627b726976f2c05857c7cbe
guy <shtar@post.bgu.ac.il>
2018-11-20 09:37:04 +0200

    adding data

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dsClass',
-    version='1.0.24',
+    version='1.0.25',
     packages=find_packages(),
     description='A useful module',
     author='Guy',
